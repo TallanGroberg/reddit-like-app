6d7b410bdada81443d5b7f44792e2d4ed6af7c10
tallan groberg <tallan.taven@gmail.com>
2019-05-15 10:15:17 -0600

    initialize

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "subs#index"
+
+  resources :subs
+
+  # get "/subs", to: "subs#index"
+  # get "/subs/:id", to: "subs#show"
+  # delete "/subs/:id", to: "subs#destroy"
+end
